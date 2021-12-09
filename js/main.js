ce19afd4675734a4c35828e8e950318b3c9ee870
unknown <you@example.com>
2021-12-09 16:43:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let h= document.querySelector('button')
+let h1 = document.querySelector('iframe')
+let img =  document.querySelector('img')
+h.onclick = function (){
+    h1.classList="show"
+    
+}
