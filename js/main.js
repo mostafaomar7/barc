8b8d8d637371db7472372ea56e1f2c0b6ab1d9a6
unknown <you@example.com>
2021-12-13 19:26:10 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 let h= document.querySelector('button')
-let h1 = document.querySelector('iframe')
-let img =  document.querySelector('img')
-h.onclick = function (){
-    h1.classList="show"
-    
-}
+// let input = document.querySelectorAll('input')
+// let p = document.querySelector('p')
+let h1 = document.querySelector('h1')
+// let h2 = document.querySelector('h2')
+let div =  document.querySelector('div')
+console.log(h);
+x=0;
+let landingpage = document.querySelector("h2")
+let imgArray = ["والله أكبر","سبحان الله   ","والحمد لله","ولا إله إلا الله "]
+document.addEventListener('click',function(){
+   x++; 
+   h1.innerHTML= ` <h1>${x}</h1> `
+   let randomNumber = Math.floor (Math.random() * imgArray.length )
+   landingpage.innerHTML=imgArray[randomNumber]
+})
+
+
+
+
+
+
